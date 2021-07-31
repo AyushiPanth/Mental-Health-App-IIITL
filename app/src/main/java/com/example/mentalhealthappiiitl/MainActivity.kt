0f9fbdde4d101b06c83d28580a80cc1f3baf3053
Rohit1173 <lcs2020048@iiitl.ac.in>
2021-07-31 09:59:55 +0530

    Fixing few bugs

--- app/src/main/java/com/example/mentalhealthappiiitl/MainActivity.kt
+++ app/src/main/java/com/example/mentalhealthappiiitl/MainActivity.kt
@@ -2,19 +2,33 @@ package com.example.mentalhealthappiiitl
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import com.example.mentalhealthappiiitl.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-   // private lateinit var binding: ActivityMainBinding
+    // private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         supportActionBar?.hide()
 
-       // binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
+        // binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
 
 
+    }
+
+    var time = 0L
+    override fun onBackPressed() {
+        if (time + 2000 > System.currentTimeMillis()) {
+            super.onBackPressed();
+        } else {
+            Toast.makeText(
+                this, "Press once again to exit",
+                Toast.LENGTH_SHORT
+            ).show();
+            time = System.currentTimeMillis();
+        }
     }
 }
